b0a955d0cc53d740e795da84ee709f62e3834d97
Firdaws Badmus <74295944+Anisah-bb@users.noreply.github.com>
2023-07-30 12:58:44 +0000

    Copilot third commit

--- member.js
+++ member.js
@@ -1,22 +1,16 @@
-function skillsMember() {
-    var member = document.getElementById("member");
-    var skills = document.getElementById("skills");
-    var projects = document.getElementById("projects");
-    var contact = document.getElementById("contact");
-    var about = document.getElementById("about");
-    var memberBtn = document.getElementById("memberBtn");
-    var skillsBtn = document.getElementById("skillsBtn");
-    var projectsBtn = document.getElementById("projectsBtn");
-    var contactBtn = document.getElementById("contactBtn");
-    var aboutBtn = document.getElementById("aboutBtn");
-    member.style.display = "block";
-    skills.style.display = "none";
-    projects.style.display = "none";
-    contact.style.display = "none";
-    about.style.display = "none";
-    memberBtn.style.backgroundColor = "#f5f5f5";
-    skillsBtn.style.backgroundColor = "#fff";
-    projectsBtn.style.backgroundColor = "#fff";
-    contactBtn.style.backgroundColor = "#fff";
-    aboutBtn.style.backgroundColor = "#fff";
+function skillsMember()
+{
+    return function (req, res, next)
+    {
+        var skills = req.body.skills;
+        if (!skills) {
+            req.body.skills = [];
+        }
+        else {
+            if (typeof skills === 'string') {
+                req.body.skills = skills.split(',');
+            }
+        }
+        next();
+    };
 }
